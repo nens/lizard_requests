7729fbb1b9de84beea335618e82ad7a71589a0ec
StevenHosper <steven.hosper@ziggo.nl>
2024-11-28 15:33:30 +0100

    Create Custom Exceptions

---
+++ src/lizard_errors.py
@@ -0,0 +1,22 @@
+class LizardPOSTError(Exception):
+    pass
+
+
+class LizardGETError(Exception):
+    pass
+
+
+class LizardPUTError(Exception):
+    pass
+
+
+class LizardPATCHError(Exception):
+    pass
+
+
+class LizardConnectionError(Exception):
+    pass
+
+
+class InvalidUrlError(Exception):
+    pass
